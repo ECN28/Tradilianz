489eda05027720fa96c03bd909204f7ee2dd364d
Ercan Yildiz <ercan_309@hotmail.de>
2021-01-02 21:22:54 +0100

    initial commit

---
+++ Tradilianz/src/main/java/com/yildiz/Tradilianz/TradilianzApplication.java
@@ -0,0 +1,13 @@
+package com.yildiz.Tradilianz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradilianzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradilianzApplication.class, args);
+	}
+
+}
---
+++ Tradilianz/src/test/java/com/yildiz/Tradilianz/TradilianzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yildiz.Tradilianz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradilianzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
