43296daddfb6588e9723a8c9aa2ffb3c9de43e14
Ercan Yildiz <ercan_309@hotmail.de>
2021-01-03 12:51:36 +0100

    fetch some data with CommandLineRunner of CustomerRepository

--- Tradilianz/src/main/java/com/yildiz/tradilianz/TradilianzApplication.java
+++ Tradilianz/src/main/java/com/yildiz/tradilianz/TradilianzApplication.java
@@ -14,20 +14,56 @@ import com.yildiz.tradilianz.customer.CustomerRepository;
 
 @SpringBootApplication
 public class TradilianzApplication {
-	
-	private static final Logger log = 
-			LoggerFactory.getLogger(TradilianzApplication.class);
+
+	private static final Logger log = LoggerFactory.getLogger(TradilianzApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradilianzApplication.class, args);
 	}
-	
+
 	@Bean
 	public CommandLineRunner demo(CustomerRepository repository) {
-		return (args) ->{
-			//save few customers
-			repository.save(new Customer("Jennifer", "Lopez", "24.07.1969", "unknown", "New York City","10100", "JenniferLopezNYC@gmail.com", "-", 2000));
-			repository.save(new Customer("Ercan", "Yildiz", "20.02.19xx", "Titaniaweg x", "Leipzig","04205", "ercan_xxx@hotmail.de", "0176217xxxx", 10));
+		return (args) -> {
+			// save few customers
+			repository.save(new Customer("Jennifer", "Lopez", "24.07.1969", "unknown", "New York City", "10100",
+					"JenniferLopezNYC@gmail.com", "-", 2000));
+			repository.save(new Customer("Ercan", "Yildiz", "20.02.19xx", "Titaniaweg x", "Leipzig", "04205",
+					"ercan_xxx@hotmail.de", "0176217xxxx", 10));
+			
+			try {
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+			}catch(NullPointerException ex) {
+				log.info(ex.getMessage());
+			}
+
+			try {
+				// fetch an individual customer by ID. if Id is not valid exception Message is thrown
+				Customer customer = repository.findById(3L);
+				log.info("Customer found with findById(1L):");
+				log.info("--------------------------------");
+				log.info(customer.toString());
+				log.info("");
+			} catch (NullPointerException ex) {
+				log.info(ex.getMessage());
+			}
+			
+			try {
+				//fetch an individual customer by EMAIL
+				Customer customer = repository.findByEmail("ercan_xxx@hotmail.de");
+				log.info("Customer found with findbyEmail(email)");
+				log.info("--------------------------------");
+				log.info(customer.toString());
+				log.info("");
+			}catch(NullPointerException ex) {
+				log.info(ex.getMessage());
+			}
+
 		};
 	}
 
--- Tradilianz/src/main/java/com/yildiz/tradilianz/customer/CustomerRepository.java
+++ Tradilianz/src/main/java/com/yildiz/tradilianz/customer/CustomerRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 	
+	/*
+	 * Here we can create our custom search queries on CustomerRepository 
+	 */
+	
 	List<Customer> findBySurname(String surname);
 	Customer findById(long id);
+	Customer findByEmail(String email);
 }
